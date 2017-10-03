a162ecd51b0b53b04dd23856378fbee422ee3447
Balakrishnan (Balki) Ranganathan <balki@live.com>
2017-10-03 11:18:21 +0200

    Service match first pass

--- AngularJS/Program.cs
+++ AngularJS/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AngularJS
 {
@@ -6,7 +11,60 @@ namespace AngularJS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CheckServiceNameDuplicates(@"C:\pe\platform\src\WebApp\scripts\app", new[] { "ts" }, "*");
+
+            if (Debugger.IsAttached)
+                Console.ReadKey();
+        }
+
+        private static void CheckServiceNameDuplicates(string path, string[] fileTypes, string filePattern = "*")
+        {
+            var files = new List<string>();
+            var services = new List<(string File, string Line, string Name)>();
+            var serviceRegex = new Regex("\\.service\\(['\"](.*)['\"]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            fileTypes
+                .ToList()
+                .ForEach(type => files.AddRange(Directory.GetFiles(path, $"{filePattern}.{type}", SearchOption.AllDirectories)));
+
+            foreach (var file in files)
+            {
+                var lines = File.ReadLines(file);
+
+                foreach (var line in lines)
+                {
+                    var matches = serviceRegex.Matches(line);
+
+                    if (matches.Any())
+                    {
+                        var name = matches[0].Groups[1].Value;
+
+                        services.Add((file, line, name));
+
+                        //Console.WriteLine($"{file}");
+                        //Console.WriteLine($"    {line}");
+                        //Console.WriteLine($"    {name}");
+                    }
+                }
+            }
+
+            foreach (var service in services)
+            {
+                var duplicates = services.Where(s => s.Name == service.Name);
+
+                var duplicateCount = duplicates.Count();
+
+                if (duplicateCount == 1)
+                    continue;
+
+                Console.WriteLine($"{service.Name}: {duplicateCount}");
+
+                foreach (var duplicate in duplicates)
+                {
+                    Console.WriteLine($"{duplicate.File}");
+                    Console.WriteLine($"    {duplicate.Line}");
+                }
+            }
         }
     }
 }
