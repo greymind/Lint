22f423bc93aaa80c7c4f278bf15d2edebd95d5a6
Balakrishnan (Balki) Ranganathan <balki@live.com>
2017-10-03 10:49:52 +0200

    Initial angular js lint commit with git ignores

---
+++ AngularJS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AngularJS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
